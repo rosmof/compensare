afd4f4bc31c50f7bc4542cb479eacaa63e2107b9
AlexandruG <alexandru@rosmof.ro>
2019-01-03 23:42:31 +0200

    home first commit

--- modules/web/src/main/java/ro/rosmof/config/RootConfig.java
+++ modules/web/src/main/java/ro/rosmof/config/RootConfig.java
@@ -3,12 +3,14 @@ package ro.rosmof.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(
-        basePackages = {"ro.rosmof"},
-        excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
+        basePackages = {"ro.rosmof.*"},
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "ro.rosmof.controller.*")})
 public class RootConfig {
 
 }
